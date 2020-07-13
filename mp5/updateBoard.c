76831180b2ce5b1b5912761ae8dd24060cbf94e2
Anthony Nguyen <alnguyn2@linux-01.ews.illinois.edu>
2020-07-13 18:06:06 -0500

    mp5

--- mp5/updateBoard.c
+++ mp5/updateBoard.c
@@ -1,5 +1,15 @@
+/* Alnguyn2 - MP5
+ * 
+ * This MP implements the Game of Life devised by John Horton Conway. It uses a 1-D array
+ * as the game board that is set with alive(1) and dead(0) cells. The game continously updates
+ * based on the adjacent cells. The game ends when there are no more changes to the board.
+ * The functions were implemented using the fact that the next board must be generated
+ * based on the current board. The current state of the board must be saved into a temp board
+ * and then updating after calculating the next state of the board.
+ * 
+ *
+ */
 
-#include "stdio.h"
 
 /* validCell - Checks (in row-major order) if the current (row,col) index is within [0, boardRowSize*boardColSize-1];
  *
@@ -28,8 +38,6 @@ int validCell(int *board, int boardRowSize, int boardColSize, int row, int col){
     return 0;
 }    
 
-
-
 /*
  * countLiveNeighbor
  * Inputs:
@@ -45,17 +53,18 @@ int validCell(int *board, int boardRowSize, int boardColSize, int row, int col){
  */
 
 int countLiveNeighbor(int* board, int boardRowSize, int boardColSize, int row, int col){
-    /* neighbors to check (X is the current index) 
+    /* neighbors to check (X is the cell to check) 
      *
      *          A B C
      *          D X E
      *          F G H
      */
 
-    /* check if all these adjacent cells are 1(alive) && valid(within range), neighbors will be incremented if both conditions hold 
+    /* check if all these adjacent cells are 1(alive) && valid(within range)
      * 
      * checking cells in the order of A,B,C, ..., G,H
      *
+     * The total neighbor count is the sum of all valid & alive neighbors
      */
     return  validCell(board, boardRowSize, boardColSize, row - 1, col - 1) +
             validCell(board, boardRowSize, boardColSize, row - 1, col) +
@@ -77,17 +86,16 @@ int countLiveNeighbor(int* board, int boardRowSize, int boardColSize, int row, i
  */
 void updateBoard(int* board, int boardRowSize, int boardColSize) {
     int i, j;
-
     int oldBoard[boardRowSize*boardColSize];
 
-    /* save the board before updating */
+    /* Save the current state since we can't update board in real-time */
     for (i = 0; i < boardRowSize*boardColSize; i++)
         oldBoard[i] = board[i];
 
-    /* check every cell */
+    /* check every cell in row-major order */
     for (i = 0; i < boardRowSize; i++)
         for (j = 0; j < boardColSize; j++){
-            /* if the current cell is alive, check neighbors */
+            /* if the current cell is alive, check its neighbors */
             if (oldBoard[i*boardColSize+j]){
                 /* current cell will die if neighbor count is not 2 or 3 */
                 if (countLiveNeighbor(board, boardRowSize, boardColSize, i, j) < 2 || countLiveNeighbor(board, boardRowSize, boardColSize, i, j) > 3)
@@ -101,7 +109,7 @@ void updateBoard(int* board, int boardRowSize, int boardColSize) {
             }
         }
 
-    /* update actual board */
+    /* update the actual board */
     for (i = 0; i < boardRowSize*boardColSize; i++)
         board[i] = oldBoard[i];
 }
@@ -119,15 +127,19 @@ void updateBoard(int* board, int boardRowSize, int boardColSize) {
  */ 
 int aliveStable(int* board, int boardRowSize, int boardColSize){
     int i;    
-    int oldBoard[boardRowSize*boardColSize];
+    int nextBoard[boardRowSize*boardColSize];
 
-    /* populate the oldBoard */
-    updateBoard(oldBoard, boardRowSize, boardColSize);
+    /* save current board to generate the next */
+    for (i = 0; i < boardRowSize*boardColSize; i++)
+        nextBoard[i] = board[i];
 
-    /* check if the current board is the same as the old board */
+    /* generate the next board */
+    updateBoard(nextBoard, boardRowSize, boardColSize);
+
+    /* check if the next board is different than the current */
     for (i = 0; i < boardRowSize*boardColSize; i++)
         /* board has been changed */
-        if (oldBoard[i] != board[i])
+        if (nextBoard[i] != board[i])
             return 0;
 
     /* board stayed the same, so we're done */
